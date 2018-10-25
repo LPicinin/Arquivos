23659f840e312247130f82956aede399cadafaff
Luís Henrique Picinin Jandre <luishpj@outlook.com.br>
2018-10-25 14:55:33 -0300

    get e set

--- arquivo.h
+++ arquivo.h
@@ -1,9 +1,71 @@
 //
 // Created by Luish on 11/10/2018.
 //
+#define null NULL
 struct Registro
 {
     int cod;
     char nome[50];
 };
 typedef struct Registro registro;
+
+void cria_arquivo(char nome[], int quantidade_de_registros)
+{
+    FILE *arq = fopen("dados.dat", "wb");//arquivo bin?rio
+    registro reg;
+    int aux_pos, i;
+    for (i = 0; i < quantidade_de_registros && quantidade_de_registros < 1000; ++i)
+    {
+        reg.cod = i;
+        sprintf(reg.nome, "nome%d", reg.cod);
+        fwrite(&reg, sizeof(registro), 1, arq);
+    }
+    fclose(arq);
+}
+int getQuantidadeRegistros(char nomearq[])
+{
+	FILE *arq = fopen("dados.dat", "rb+");
+	if(arq != null)
+	{
+		fseek(arq, 0, 2);
+		return ftell(arq) / sizeof(registro);
+	}
+	else
+		return -1;
+}
+registro get(int n, char nomearq[])
+{
+	FILE *arq = fopen("dados.dat", "rb+");
+	registro reg;
+	if(arq != null)
+	{
+		fseek(arq, sizeof(registro)*n, 0);
+		fread(&reg,sizeof(registro), 1, arq);
+		fclose(arq);
+	}
+	return reg;
+}
+void set(int n, char nomearq[], registro r)
+{
+	FILE *arq = fopen("dados.dat", "rb+");
+	if(arq != null)
+	{
+		fseek(arq, sizeof(registro)*n, 0);
+		fwrite(&r,sizeof(registro), 1, arq);
+		fclose(arq);
+	}
+}
+void exibe(char nome[])
+{
+	FILE *arq = fopen("dados.dat", "rb");
+	registro r;
+	if(arq != null)
+	{
+		do
+		{
+			fread(&r, sizeof(registro), 1, arq);
+			printf("cod = %d, nome = %s\n", r.cod, r.nome);
+		}while(!feof(arq));
+		fclose(arq);
+	}
+}
--- main.c
+++ main.c
@@ -3,33 +3,19 @@
 #include <string.h>
 #include <time.h>
 #include "arquivo.h"
-#define null NULL
-void cria_arquivo(char nome[], int quantidade_de_registros)
-{
-    FILE *arq = fopen("dados.dat", "wb");//arquivo binário
-    FILE *narq = fopen("nomes.txt", "r");//arquivo texto +-1000 nomes
-    registro reg;
-    char aux_nome[50];
-    int aux_pos;
-    if(narq != null)
-    {
-        for (int i = 0; i < quantidade_de_registros && quantidade_de_registros < 1000; ++i)
-        {
-            aux_pos = rand()%1000;
-            fseek(arq, sizeof(registro)*aux_pos, 0);
-            fgets(aux_nome, 50, arq);
-            reg.cod = rand()%quantidade_de_registros;
-            strcpy(reg.nome, aux_nome);
-            fwrite(&reg, sizeof(registro), 1, arq);
-        }
-    }
-    fclose(arq);
-    fclose(narq);
-}
+
+
 int main()
 {
     srand(time(null));
-    cria_arquivo("teste.dat", 50);
-    printf("hum");
+    //cria_arquivo("teste.dat", 50);
+    //printf("%d", getQuantidadeRegistros("teste.dat"));//pega tf do arquivo
+    registro r2;
+    r2.cod = 78;
+    strcpy(r2.nome, "teste");
+    set(3, "teste.dat", r2);
+    registro r = get(3, "teste.dat");//pega o elemento da posicao informada
+    printf("%d, %s", r.cod, r.nome);
+    //exibe("teste.dat");
     return 0;
 }
